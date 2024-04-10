3539a9339e79ffcd5310870dc4418a183b526672
AjaykumarKavuru <ajay770261@gmail.com>
2024-04-10 12:01:49 +0530

    code updated

--- src/main/java/Selenium/TestBase.java
+++ src/main/java/Selenium/TestBase.java
@@ -3,6 +3,7 @@ package Selenium;
 import org.openqa.selenium.WebDriver;
 //import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 
 public class TestBase {
 
@@ -11,6 +12,9 @@ public class TestBase {
 	public void launchBrowser() {
 //		System.setProperty("Webdriver.chrome.driver", "C:\\\\\\\\Users\\\\\\\\ajay7\\\\\\\\Downloads\\\\\\\\chromedriver-win64\\\\\\\\chromedriver-win64\\\\\\\\chromedriver.exe");
 //		driver = new ChromeDriver();
-		driver = new EdgeDriver();
+		EdgeOptions opt = new EdgeOptions();          
+        opt.addArguments("headless");
+  
+        driver = new EdgeDriver(opt);
 	}
 }
