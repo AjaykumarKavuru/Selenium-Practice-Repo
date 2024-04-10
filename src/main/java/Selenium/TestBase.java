ac9349bea654c23a7d67ce1bea95c25b7b6facae
AjaykumarKavuru <ajay770261@gmail.com>
2024-04-10 11:16:28 +0530

    code updated

--- src/main/java/Selenium/TestBase.java
+++ src/main/java/Selenium/TestBase.java
@@ -1,14 +1,16 @@
 package Selenium;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class TestBase {
 
 	public static WebDriver driver;
 	
 	public void launchBrowser() {
-		System.setProperty("Webdriver.chrome.driver", "C:\\\\\\\\Users\\\\\\\\ajay7\\\\\\\\Downloads\\\\\\\\chromedriver-win64\\\\\\\\chromedriver-win64\\\\\\\\chromedriver.exe");
-		driver = new ChromeDriver();
+//		System.setProperty("Webdriver.chrome.driver", "C:\\\\\\\\Users\\\\\\\\ajay7\\\\\\\\Downloads\\\\\\\\chromedriver-win64\\\\\\\\chromedriver-win64\\\\\\\\chromedriver.exe");
+//		driver = new ChromeDriver();
+		driver = new EdgeDriver();
 	}
 }
